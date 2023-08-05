dfa4c6a84f0df2956d3e653f6ee56fcda686278a
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-08-05 16:42:39 +0300

    done

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,12 +1,24 @@
 package com.epam.rd.autotasks;
 
-
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
-
-        return null;
+        return Collections.list(new StringTokenizer(lines.toString(),
+                        "[] .,‘’(“—/:?!”;*)'\\\"-")).stream()
+                .map(t -> (String) t)
+                .map(String::toLowerCase)
+                .filter(t -> t.length() > 3)
+                .collect(Collectors.groupingBy(Function.identity(), HashMap::new,
+                        Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .filter(t -> t.getValue() > 9)
+                .map(e -> e.getKey() + " - " + e.getValue())
+                .collect(Collectors.joining("\n"));
     }
 }
